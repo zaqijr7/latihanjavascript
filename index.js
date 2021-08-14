356f593049c0e349773a794c62db18d1773ed5e2
zaqijr7 <zaqijr7@gmail.com>
2021-08-14 19:50:07 +0700

    project pertama saya

---
+++ index.js
@@ -0,0 +1,7 @@
+const nama = "muhammad zaqi"; // Ini variabel dengan tipe data string
+console.log(`Nama saya adalah ${nama}`);
+const nilai1 = 20; // ini variabel number
+const nilai2 = 230;
+const Jumlah = nilai1 + nilai2
+
+console.log('Jumlah antara nilai 1 dan nilai 2 adalah : ' + Jumlah);
